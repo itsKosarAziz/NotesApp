6a8d7e3265bce26cf884353beb0e32fa3e8f15af
itsKosarAz <azizkosar17@gmail.com>
2024-02-29 19:34:30 +0300

    Added stream controller to ensure that changes in file will be reloaded continuously and fixed the null checking along with error handlings

--- flutter_notes_app/lib/notesPage.dart
+++ flutter_notes_app/lib/notesPage.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -10,35 +11,73 @@ class notesPage extends StatefulWidget {
 }
 
 class _notesPageState extends State<notesPage> {
-  List _notes = [];
+  final StreamController<List<dynamic>> _notesStreamController =
+      StreamController<List<dynamic>>();
+  bool loadingNotes = true;
+
+  @override
+  void initState() {
+    super.initState();
+    getNotes();
+  }
+
   Future<void> getNotes() async {
-    final String notesResponse =
-        await rootBundle.loadString('assets/notes.json');
-    final notesData = await json.decode(notesResponse);
-    setState(() {
-      _notes = notesData["notes"];
-    });
+    try {
+      final String notesResponse =
+          await rootBundle.loadString('assets/notes.json');
+      final List<dynamic> notesData = json.decode(notesResponse)['notes'];
+      _notesStreamController.add(notesData);
+    } catch (e) {
+      _notesStreamController.addError('Could not load notes');
+    } finally {
+      setState(() {
+        loadingNotes = false;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    getNotes();
     return Scaffold(
         appBar: AppBar(
           title: const Text('Notes App'),
           backgroundColor: Colors.deepPurple,
         ),
-        body: Expanded(
-            child: ListView.builder(
-          itemCount: _notes.length,
-          itemBuilder: (context, index) {
-            return Card(
-                key: ValueKey(_notes[index]["id"]),
-                child: ListTile(
-                  leading: Text(_notes[index]["title"]),
-                  title: Text(_notes[index]["description"]),
-                ));
-          },
-        )));
+        body: StreamBuilder<List<dynamic>>(
+            stream: _notesStreamController.stream,
+            builder: (context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return Center(child: CircularProgressIndicator());
+              } else if (snapshot.hasError) {
+                return Center(child: Text('${snapshot.error}'));
+              } else if (snapshot.hasData) {
+                final List<dynamic> notes = snapshot.data!;
+                if (notes.isNotEmpty) {
+                  return ListView.builder(
+                      itemCount: notes.length,
+                      itemBuilder: (context, index) {
+                        return Card(
+                          key: ValueKey(notes[index]["id"]),
+                          child: ListTile(
+                            leading: Text(notes[index]["title"]),
+                            title: Text(notes[index]["description"]),
+                          ),
+                        );
+                      });
+                } else {
+                  return Center(
+                      child: Text(
+                          'No notes to load, please add a note to the file'));
+                }
+              } else {
+                return Center(child: CircularProgressIndicator());
+              }
+            }));
+  }
+
+  @override
+  void dispose() {
+    _notesStreamController.close();
+    super.dispose();
   }
 }
